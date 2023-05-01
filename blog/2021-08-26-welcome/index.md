d13bb016151da2d56e88f7defc64df8a4dfb736a
aleksei <robby.roboter@gmail.com>
2023-05-01 12:26:16 +0300

    build on server

--- blog/2021-08-26-welcome/index.md
+++ blog/2021-08-26-welcome/index.md
@@ -1,7 +1,7 @@
 ---
 slug: welcome
 title: Welcome
-authors: [slorber, yangshun]
+authors: [roboter]
 tags: [facebook, hello, docusaurus]
 ---
 
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/roboter',
             label: 'GitHub',
             position: 'right',
           },
@@ -122,7 +122,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/roboter',
               },
             ],
           },
